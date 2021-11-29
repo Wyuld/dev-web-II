4b05cc903a2debf6254ddd49938290cba1c15818
Wyuld <brayanluiz144@gmail.com>
2021-11-29 15:27:10 -0500

    Created Models

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from core.models import Empregador,VagaDeEmprego, Inscricao
+
+admin.site.register(Empregador)
+admin.site.register(VagaDeEmprego)
+admin.site.register(Inscricao)
---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.db.models.deletion import PROTECT
+from django.contrib.auth.models import User
+
+class Empregador(models.Model):
+    class Meta:
+        verbose_name_plural="Empregadores"
+    razaoSocial = models.CharField(max_length=255)
+    cnpj = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.razaoSocial
+
+class VagaDeEmprego(models.Model):
+    class Meta:
+        verbose_name_plural="Vagas de Emprego"
+    titulo = models.CharField(max_length=255)
+    descricao = models.CharField(max_length=255)
+    empregador = models.ForeignKey(Empregador, on_delete=PROTECT, related_name="empregos")
+
+class Inscricao(models.Model):
+    class Meta:
+        verbose_name_plural="Inscricoes"
+    usuario = models.ForeignKey(User, on_delete=PROTECT, related_name="inscricoes")
+    empregador = models.ForeignKey(Empregador, on_delete=PROTECT, related_name="inscricoes", default=" ")
